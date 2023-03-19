43e28e61e33cad8ee35cfe9a2f5cce657636be48
Soham Agarwal <92148394+soham-agarwal@users.noreply.github.com>
2023-03-19 19:50:14 +0530

    all shoes sorted webpages

--- src/components/adidas.js
+++ src/components/adidas.js
@@ -1,31 +1,103 @@
 import React from 'react'
+import Product from "./Product"
 
 function Adidas() {
     return (
         <div>
             <div className="home__row">
-        <Product
-          id="11"
-          title="JORDAN 1 LOW MULTI-COLOR (W)"
-          price={19900}
-    
-          image="https://mikaandmia.com/wp-content/uploads/2022/11/Jordan-1-Low-SE-Multi-Color-W-PhotoRoom.png"
-        />
-        <Product
-          id="13"
-          title="NIKE DUNK LOW BLUE SASHIKO"
-          price={20000}
-    
-          image="https://images.stockx.com/images/Nike-Dunk-Low-Industrial-Blue-Sashiko-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&dpr=2&trim=color&updated_at=1676277261&q=75"
-        />
-        <Product
-          id="13"
-          title="NIKE DUNK LOW BLUE SASHIKO"
-          price={20000}
-    
-          image="https://images.stockx.com/images/Nike-Dunk-Low-Industrial-Blue-Sashiko-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&dpr=2&trim=color&updated_at=1676277261&q=75"
-        />
-      </div></div>
+            <Product
+            id="21"
+            title="STREET ICON"
+            price={4000}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/bea682ea358743599a76afb200d99c10_9366/STREET_ICON_M_Black_GC0660_01_standard.jpg"
+            />
+            <Product
+            id="22"
+            title="FORUM LOW"
+            price={10800}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/75aee7eb0fc343d484c9adf30019d225_9366/Forum_Low_Shoes_White_GY8556_01_standard.jpg"
+          />
+          <Product
+            id="23"
+            title="SKATEBOARDING SUEDE"
+            price={5999}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/439c316b2caf4bf499b3aeba00160a61_9366/Daily_3.0_Lifestyle_Skateboarding_Suede_Shoes_Red_GY2252_01_standard.jpg"
+          />
+          </div>
+          <div className="home__row">
+             <Product
+            id="21"
+            title="FORUM EXHBIT"
+            price={9999}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/b19534d0b69a4d0f83c7af230161ead6_9366/Forum_Exhibit_Low_Shoes_Grey_IF9956_01_standard.jpg"
+          />
+            <Product
+            id="22"
+            title="BREAKNET 2.0"
+            price={12900}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/d9cbf93890554349a33eaf8100cc02f1_9366/Breaknet_2.0_Shoes_White_HP9451_01_standard.jpg"
+          />
+          <Product
+            id="23"
+            title="CENNENTIAL 85"
+            price={10999}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/dcf06aa9374c407bb133af0f00214ae6_9366/Centennial_85_Low_Shoes_White_ID1812_01_standard.jpg"
+          />
+        </div>
+        <div className="home__row">
+             <Product
+            id="21"
+            title="JEREMY SCOTT OPAL 4.0 SHOES
+            "
+            price={21999}
+
+            image="https://assets.adidas.com/images/w_600,f_auto,q_auto/6a5aea66fb3b45478113af500018a847_9366/Jeremy_Scott_Opal_Wings_4.0_Shoes_Black_IE6862_01_standard.jpg"
+          />
+            <Product
+            id="22"
+            title="PUREBOOST 22 SHOES"
+            price={12900}
+
+            image="https://assets.adidas.com/images/w_600,f_auto,q_auto/8024f2fd73864c04a28daf4701571e8e_9366/Pureboost_22_Shoes_White_HQ8585_01_standard.jpg"
+          />
+          <Product
+            id="23"
+            title="ADIDAS X MARIMEKKO SHOES"
+            price={11999}
+
+            image="https://assets.adidas.com/images/w_600,f_auto,q_auto/c61a24587171400fba8daf8800e9a5c3_9366/adidas_x_Marimekko_Superstar_Shoes_White_HP9779_01_standard.jpg"
+          />
+        </div>
+        <div className="home__row">
+             <Product
+            id="21"
+            title="ADI2000"
+            price={10999}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/34a88a6f03b243499013af2a00b6bf78_9366/Adi2000_Shoes_Red_H03487_01_standard.jpg"
+          />
+            <Product
+            id="22"
+            title="RIVARLY LOW"
+            price={11900}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/cc1cae2a26304f8b87daaf5e00e628f7_9366/Rivalry_Low_86_Shoes_Grey_HQ7017_01_standard.jpg"
+          />
+          <Product
+            id="23"
+            title="ADIDAS X DISNEY"
+            price={8000}
+
+            image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/32982002d55e435795d3ae8a00cb99d5_9366/adidas_x_Disney_Advantage_Muppets_Lace_Shoes_White_GY6587_01_standard.jpg"
+          />
+        </div>
+        </div>
     )
 }
 
--- src/components/nike.js
+++ src/components/nike.js
@@ -1,32 +1,102 @@
 import React from 'react'
 import Product from "./Product"
-  
+
 function Nike() {
   return (
     <div>
       <div className="home__row">
-    <Product
-      id="11"
-      title="JORDAN 1 LOW MULTI-COLOR (W)"
-      price={19900}
+        <Product
+          id="11"
+          title="NIKE AIRFORCE'07 "
+          price={10900}
 
-      image="https://mikaandmia.com/wp-content/uploads/2022/11/Jordan-1-Low-SE-Multi-Color-W-PhotoRoom.png"
-    />
-    <Product
-      id="13"
-      title="NIKE DUNK LOW BLUE SASHIKO"
-      price={20000}
+          image="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4e890856-b6ce-492b-83f5-9aca6e147ad8/air-force-1-07-lv8-shoes-Blj3sx.png"
+        />
+        <Product
+          id="13"
+          title="NIKE AJ1 MIDSE"
+          price={12925}
 
-      image="https://images.stockx.com/images/Nike-Dunk-Low-Industrial-Blue-Sashiko-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&dpr=2&trim=color&updated_at=1676277261&q=75"
-    />
-    <Product
-      id="13"
-      title="NIKE DUNK LOW BLUE SASHIKO"
-      price={20000}
+          image="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/0d6a88ca-7ff8-4ad9-991b-b2e2a33cf66e/air-jordan-1-mid-se-shoes-CQ6f9G.png"
+        />
+        <Product
+          id="13"
+          title="NIKE AIR DELDON"
+          price={11000}
 
-      image="https://images.stockx.com/images/Nike-Dunk-Low-Industrial-Blue-Sashiko-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&dpr=2&trim=color&updated_at=1676277261&q=75"
-    />
-  </div></div>
+          image="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/61bf94c2-eee8-4f4e-895f-53e68a1fe930/air-deldon-hoodie-basketball-shoes-L07kGx.png"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="11"
+          title="NIKE VAPOURMAX"
+          price={13000}
+
+          image="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/3c7f0a74-9376-4b50-a410-5795807e2845/dunk-high-retro-shoes-WGkPW2.png"
+        />
+        <Product
+          id="13"
+          title="NIKE BLAZER MID"
+          price={10000}
+
+          image="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/6eea83ac-7862-459e-abf5-2f566e2f0ac1/blazer-mid-77-vintage-shoes-d8ZCkw.png"
+        />
+        <Product
+          id="13"
+          title="NIKE LOW DUNK"
+          price={9225}
+
+          image="https://images.stockx.com/images/Nike-Dunk-Low-Retro-QS-Flash-White-Argon-Blue-Flash-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&dpr=2&trim=color&updated_at=1658842672&q=75"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="13"
+          title="NIKE DUNK LOW BLUE SASHIKO"
+          price={20000}
+
+          image="https://images.stockx.com/images/Nike-Dunk-Low-Industrial-Blue-Sashiko-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&dpr=2&trim=color&updated_at=1676277261&q=75"
+        />
+        <Product
+          id="11"
+          title="NIKE ZION HP"
+          price={10000}
+
+          image="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/693cc53f-d228-472a-bf17-0a2e2c42daf7/air-jordan-1-mid-se-craft-shoes-RmP7J6.png"
+        />
+        <Product
+          id="11"
+          title="JORDAN 1 LOW MULTI-COLOR (W)"
+          price={19900}
+
+          image="https://mikaandmia.com/wp-content/uploads/2022/11/Jordan-1-Low-SE-Multi-Color-W-PhotoRoom.png"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="11"
+          title="NIKE JUNIPER"
+          price={7050}
+
+          image="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/b24aea97-f10e-40b2-91eb-2bc027a4dd89/juniper-trail-2-next-nature-trail-running-shoes-PT4wqk.png"
+        />
+        <Product
+          id="13"
+          title="NIKE COSMIC 2"
+          price={13000}
+
+          image="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/c0642094-8cfc-4850-ab9a-55b7e2f6f755/cosmic-unity-2-basketball-shoes-S6n2s3.png"
+        />
+        <Product
+          id="13"
+          title="NIKE AJ1 CRAFT"
+          price={14225}
+
+          image="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/312c82cd-03f7-4b38-97bb-37348c415250/zion-2-pf-basketball-shoes-pwN2Q4.png"
+        />
+      </div>
+    </div>
   )
 }
 
