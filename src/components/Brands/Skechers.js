acce90e46c3bd974d237307c9f18722568476b3d
vedant jadhav <jadhavedant12@gmail.com>
2023-04-01 17:50:30 +0530

    shoes added

--- src/components/Brands/Skechers.js
+++ src/components/Brands/Skechers.js
@@ -29,71 +29,70 @@ function Skechers() {
             <div className="home__row">
                 <Product
                     id="21"
-                    title="FORUM EXHBIT"
-                    price={9999}
+                    title="SKECHERS MONSTER"
+                    price={4999}
 
-                    image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/b19534d0b69a4d0f83c7af230161ead6_9366/Forum_Exhibit_Low_Shoes_Grey_IF9956_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dwf7041bf2/images/large/194880079390-5.jpg"
                 />
                 <Product
                     id="22"
-                    title="BREAKNET 2.0"
-                    price={12900}
+                    title="D'LITES FRESH AIR"
+                    price={5599}
 
-                    image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/d9cbf93890554349a33eaf8100cc02f1_9366/Breaknet_2.0_Shoes_White_HP9451_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw213823f8/images/large/195204393376-5.jpg"
                 />
                 <Product
                     id="23"
-                    title="CENNENTIAL 85"
-                    price={10999}
+                    title="ARCH FIT D'LUX - JUNCTION"
+                    price={5599}
 
-                    image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/dcf06aa9374c407bb133af0f00214ae6_9366/Centennial_85_Low_Shoes_White_ID1812_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dwc21b7d5b/images/large/196642227575-5.jpg"
                 />
             </div>
             <div className="home__row">
                 <Product
                     id="21"
-                    title="JEREMY SCOTT OPAL 4.0 SHOES
-            "
-                    price={21999}
+                    title="Ultra felx 2"
+                    price={6499}
 
-                    image="https://assets.adidas.com/images/w_600,f_auto,q_auto/6a5aea66fb3b45478113af500018a847_9366/Jeremy_Scott_Opal_Wings_4.0_Shoes_Black_IE6862_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw2fb341e4/images/large/195204713549-5.jpg"
                 />
                 <Product
                     id="22"
-                    title="PUREBOOST 22 SHOES"
-                    price={12900}
+                    title="SKECH-AIR ELEMENT"
+                    price={7900}
 
-                    image="https://assets.adidas.com/images/w_600,f_auto,q_auto/8024f2fd73864c04a28daf4701571e8e_9366/Pureboost_22_Shoes_White_HQ8585_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw6a0fceb2/images/large/195204006689-5.jpg"
                 />
                 <Product
                     id="23"
-                    title="ADIDAS X MARIMEKKO SHOES"
-                    price={11999}
+                    title="UNO-IN CAMO NEATO"
+                    price={8499}
 
-                    image="https://assets.adidas.com/images/w_600,f_auto,q_auto/c61a24587171400fba8daf8800e9a5c3_9366/adidas_x_Marimekko_Superstar_Shoes_White_HP9779_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dwee9645f0/images/large/195969908761-5.jpg"
                 />
             </div>
             <div className="home__row">
                 <Product
                     id="21"
-                    title="ADI2000"
-                    price={10999}
+                    title="GO walk 6 "
+                    price={7999}
 
-                    image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/34a88a6f03b243499013af2a00b6bf78_9366/Adi2000_Shoes_Red_H03487_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dwb815b643/images/large/196642721622-5.jpg"
                 />
                 <Product
                     id="22"
-                    title="RIVARLY LOW"
-                    price={11900}
+                    title="Power-VOLT"
+                    price={6900}
 
-                    image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/cc1cae2a26304f8b87daaf5e00e628f7_9366/Rivalry_Low_86_Shoes_Grey_HQ7017_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dwcfcbdc0d/images/large/194880321796-5.jpg"
                 />
                 <Product
                     id="23"
-                    title="ADIDAS X DISNEY"
-                    price={8000}
+                    title="Go Run Elevate"
+                    price={4899}
 
-                    image="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/32982002d55e435795d3ae8a00cb99d5_9366/adidas_x_Disney_Advantage_Muppets_Lace_Shoes_White_GY6587_01_standard.jpg"
+                    image="https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw460ac0e0/images/large/196642719964-5.jpg"
                 />
             </div>
         </div>
